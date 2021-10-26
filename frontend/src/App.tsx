8fab5ff9d3cb065a3e3288f83677c163130766ed
fjimenez81 <fernancuerva@gmail.com>
2021-10-26 17:34:19 +0200

    add Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
 import './App.css';
+
+/*COMPONENTS*/
+
+import { HomePage } from './components/mainPage/Home/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Redirect to="/" />
+        </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/mainPage/Home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homePage.css"
+
+export const HomePage = () => {
+    return (
+        <div>
+            <div className="container">
+                <h1>MATCHA</h1>
+            </div>
+        </div>
+    )
+}
